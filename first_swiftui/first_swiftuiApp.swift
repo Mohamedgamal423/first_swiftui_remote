c7d93fe1ef0261a19a85960f0072c30a4d461da7
Mohamedgamal423 <69890404+Mohamedgamal423@users.noreply.github.com>
2023-07-07 09:03:29 -0700

    Initial Commit

---
+++ first_swiftui/first_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_swiftuiApp.swift
+//  first_swiftui
+//
+//  Created by Mohamed on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct first_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
