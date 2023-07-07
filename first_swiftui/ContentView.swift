5447c095a44524a588731566252bddd950657d14
Mohamedgamal423 <69890404+Mohamedgamal423@users.noreply.github.com>
2023-07-07 09:15:01 -0700

    change text content

--- first_swiftui/ContentView.swift
+++ first_swiftui/ContentView.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
+// first app
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hey mohamed ios")
         }
         .padding()
     }
